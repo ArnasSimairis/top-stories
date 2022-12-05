7ce30b8a7af04c5415f3873f34f16d6f1c6a5f9b
Moksleivis <Moksleivis@kitm.local>
2022-12-05 13:55:47 +0200

    finished

--- srcipt.js
+++
@@ -1,5 +0,0 @@
-fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=G6UAg1ph0qbH6GLpPHKtqGiuzwIM7eIm')
- .then(Response => Response.json())
- .then(commits => {
-    console.log(commits.results);
- })
